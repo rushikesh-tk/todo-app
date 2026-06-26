36af36bcebde4f482aa22a93502ed4a3bc207b19
Rushikesh <rushikeshtaksande7@gmail.com>
2026-06-26 13:17:21 +0530

    feat: implement todos routes and wired main.py and updated requirements.txt

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,25 @@
-# FastAPI app entry point, mounts routers
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from routers import auth, todos
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+API_PREFIX = "/api/v1"
+
+app.include_router(
+    auth.router, prefix=f"{API_PREFIX}/auth", tags=["Authentication"])
+app.include_router(todos.router, prefix=f"{API_PREFIX}/todos", tags=["Todos"])
+
+
+@app.get("/")
+def health():
+    return {"status": "healthy"}
--- backend/routers/todos.py
+++ backend/routers/todos.py
@@ -1,1 +1,52 @@
-# GET/POST/PUT/DELETE /todos
+from fastapi import APIRouter, Depends, HTTPException, status
+from models.todo import TodoCreate, TodoUpdate, TodoOut
+from typing import Annotated
+from core.security import get_current_user
+from core.database import get_db
+from db.queries.todo_queries import get_todos_by_user, create_todo, update_todo, delete_todo
+
+router = APIRouter()
+
+
+@router.get("/", response_model=list[TodoOut])
+async def get_all_users_todos(user_id: Annotated[int, Depends(get_current_user)]):
+    with get_db() as cursor:
+        todos = get_todos_by_user(cursor, user_id)
+        return todos
+
+
+@router.post("/", response_model=TodoOut, status_code=status.HTTP_201_CREATED)
+async def create_new_todo(todo_data: TodoCreate, user_id: Annotated[int, Depends(get_current_user)]):
+    with get_db() as cursor:
+        new_todo = create_todo(
+            cursor, user_id, todo_data.title, todo_data.priority)
+        if not new_todo:
+            raise HTTPException(
+                status_code=500, detail="Failed to create todo")
+        return TodoOut(**new_todo)
+
+
+@router.put("/{todo_id}", response_model=TodoOut)
+async def update_current_todo(todo_id: int, todo_data: TodoUpdate, user_id: Annotated[int, Depends(get_current_user)]):
+    with get_db() as cursor:
+        if not todo_data.model_dump(exclude_none=True):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail="No fields provided to update")
+        updated_todo = update_todo(
+            cursor, todo_id, user_id, **todo_data.model_dump(exclude_none=True))
+        if not updated_todo:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
+
+        return TodoOut(**updated_todo)
+
+
+@router.delete("/{todo_id}")
+async def delete_current_todo(todo_id: int, user_id: Annotated[int, Depends(get_current_user)]):
+    with get_db() as cursor:
+        is_deleted = delete_todo(cursor, todo_id, user_id)
+        if not is_deleted:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
+
+        return {"message": "Todo deleted successfully"}
